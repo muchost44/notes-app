0f31b7bbdb95e73942e614a30c4277944683533f
muchost44 <musyaffa220@gmail.com>
2022-08-23 16:12:38 +0700

    buat halaman

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ListView from "../views/ListView.vue";
+import NoteView from "../views/NoteView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/note",
+    name: "note",
+    component: NoteView,
+  },
+  {
+    path: "/list",
+    name: "list",
+    component: ListView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
